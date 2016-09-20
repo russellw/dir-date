13b931804f81d18165d36f7d3c712103ec4f5b7a
Russell Wallace <russell.wallace@gmail.com>
2016-09-20 14:43:42 +0100

    project

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dir_date
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

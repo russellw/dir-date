23c10a3c4592af90a518a269a5403a8d07bc3a31
Russell Wallace <russell.wallace@gmail.com>
2023-11-19 12:28:28 +0000

    main

--- Program.cs
+++ Program.cs
@@ -1,5 +1,56 @@
 internal class Program {
 	static void Main(string[] args) {
-		Console.WriteLine("Hello, World!");
+		var options = true;
+		var paths = new List<string>();
+		foreach (var arg in args) {
+			var s = arg;
+			if (options) {
+				if (s == "--") {
+					options = false;
+					continue;
+				}
+				if (s.StartsWith('-')) {
+					while (s.StartsWith('-'))
+						s = s[1..];
+					switch (s) {
+					case "?":
+					case "h":
+					case "help":
+						Help();
+						return;
+					case "V":
+					case "v":
+					case "version":
+						Console.WriteLine("dir-date " + typeof(Program).Assembly.GetName()?.Version?.ToString(2));
+						return;
+					default:
+						Console.WriteLine(arg + ": unknown option");
+						Environment.Exit(1);
+						break;
+					}
+				}
+			}
+			paths.Add(s);
+		}
+		if (paths.Count == 0)
+			paths.Add(".");
+
+		foreach (var path in paths)
+			Do(path);
+	}
+
+	static void Help() {
+		Console.WriteLine("Usage: dir-date [options] [path...]");
+		Console.WriteLine();
+		Console.WriteLine("-h  Show help");
+		Console.WriteLine("-V  Show version");
+	}
+
+	static void Do(string path) {
+		if (Directory.Exists(path))
+			foreach (var entry in Directory.GetFileSystemEntries(path))
+				Do(entry);
+		else
+			Console.WriteLine(path);
 	}
 }
